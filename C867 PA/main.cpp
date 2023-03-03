b620dd7c360b315ca20bf52decb9954a74126e4f
David Praseuth <davidrpraseuth@gmail.com>
2023-03-03 08:07:39 -0800

    Initial Commit

---
+++ C867 PA/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C867 PA
+//
+//  Created by David Praseuth on 3/3/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
